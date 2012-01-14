3088bc94e0b0a1771212422ee03450c2e27943ff
Jean-Nicolas Boulay Desjardins <jnbdzjnbdz@gmail.com>
2012-01-14 00:03:33 -0500

    Change the x and y to make them uppercase. It's more readable.

--- Source/Element.serializeArea.js
+++ Source/Element.serializeArea.js
@@ -21,10 +21,10 @@ Element.implement({
         var coors = this.getCoordinates(relative);
         return {
                 el: this,
-                lefty: coors.top.toInt(),
-                leftx: coors.left.toInt(),
-                righty: coors.height.toInt() + coors.top.toInt(),
-                rightx: coors.width.toInt() + coors.left.toInt()
+                leftY: coors.top.toInt(),
+                leftX: coors.left.toInt(),
+                rightY: coors.height.toInt() + coors.top.toInt(),
+                rightX: coors.width.toInt() + coors.left.toInt()
             };
     }
 });
